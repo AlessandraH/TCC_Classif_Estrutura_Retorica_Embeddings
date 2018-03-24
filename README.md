966aa84e1454bd4439dcdcf4873d897417edfcff
AlessandraH <alehairi@gmail.com>
2018-03-24 14:27:32 -0300

    adicionando corpus e funcoes basicas

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TCC_Classif_Estrutura_Retorica_Embeddings
+## Universidade Estadual de Maringá
+## Trabalho de Conclusão de Curso 2017-2018
+Extração de características para a classificação de estrutura retórica em resumos científicos
---
+++ classificador_sentenca_embeddings.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import json
+import numpy as np
+import time
+import fasttext
+
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import cross_val_predict
+from sklearn.feature_selection import SelectKBest, chi2
+from scipy.sparse import hstack
+from sklearn.svm import LinearSVC
+from sklearn import neighbors
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from gensim.models import KeyedVectors
+
+
+def to_sentences(abstracts, senteces_max=None):
+    sentences = []
+    labels = []
+    abstracts_sentences = []
+    abstracts_labels = []
+    ids = []
+
+    for id, abstract in enumerate(abstracts):
+        if senteces_max and len(abstract) > senteces_max:
+            continue
+
+        tmp_sentences = []
+        tmp_labels = []
+
+        for label, text in abstract:
+            sentences.append(text)
+            labels.append(label)
+
+            tmp_sentences.append(text)
+            tmp_labels.append(label)
+            ids.append(id)
+
+        abstracts_sentences.append(tmp_sentences)
+        abstracts_labels.append(tmp_labels)
+
+    assert (len(sentences) == len(labels))
+    assert (len(abstracts_sentences) == len(abstracts_labels))
+
+    return sentences, labels, abstracts_sentences, abstracts_labels, ids
+
+
+def loadFromJson(file):
+    data = []
+    with open(file, 'r') as f:
+        data = json.load(f, encoding='cp1252')
+
+    return to_sentences(data)
+
+
+def abstracts_to_sentences(abstracts, labels):
+    ret = []
+    ret_prev = []
+    ret_next = []
+    ret_labels = []
+    ret_pos = []
+    abstracts_idx = []
+
+    for i, (sentences_labels, sentences) in enumerate(zip(labels, abstracts)):
+        for j, (label, sentence) in enumerate(zip(sentences_labels, sentences)):
+            ret.append(sentence)
+            ret_pos.append(j)
+            ret_labels.append(label)
+            abstracts_idx.append(i)
+
+            if j - 1 >= 0:
+                ret_prev.append(sentences[j - 1])
+            else:
+                ret_prev.append("")
+
+            if j + 1 < len(sentences):
+                ret_next.append(sentences[j + 1])
+            else:
+                ret_next.append("")
+
+    return ret, ret_prev, ret_next, ret_pos, ret_labels, abstracts_idx
+
+
+def classificador():
+    print(time.asctime(time.localtime(time.time())))
+    model = KeyedVectors.load_word2vec_format(fname='glove_s50.txt', binary=False, unicode_errors="ignore")
+    print(time.asctime(time.localtime(time.time())))
+
+
+classificador()
