42c2a158f3532c89f9dd53d68b3267c46006a51f
Alessandra H. Iriguti <ra88741@uem.br>
2018-04-11 22:57:49 -0300

    tentando usar código retirado d eblog

--- classificador_sentenca_embeddings.py
+++ classificador_sentenca_embeddings.py
@@ -2,9 +2,7 @@
 
 import time
 import json
-from collections import defaultdict
 
-import scipy
 import numpy as np
 import sklearn_crfsuite
 
@@ -22,54 +20,6 @@ from gensim.models import KeyedVectors
 
 pontuacao = ['.', ',', ' ', '"', '!', '(', ')', '-', '=', '+', '/', '*', ';', ':',
                 '[', ']', '{', '}', '$', '#', '@', '%', '&', '?']
-
-
-# classe retirada de http://nadbordrozd.github.io/blog/2016/05/20/text-classification-with-word2vec/
-class MeanEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        # if a text is empty we should return a vector of zeros
-        # with the same dimensionality as all the other vectors
-        self.dim = len(word2vec.itervalues().next())
-
-    def fit(self, X, y):
-        return self
-
-    def transform(self, X):
-        return np.array([
-            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-                    or [np.zeros(self.dim)], axis=0)
-            for words in X
-        ])
-
-
-# classe retirada de http://nadbordrozd.github.io/blog/2016/05/20/text-classification-with-word2vec/
-class TfidfEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        self.word2weight = None
-        self.dim = len(word2vec.itervalues().next())
-
-    def fit(self, X, y):
-        tfidf = TfidfVectorizer(analyzer=lambda x: x)
-        tfidf.fit(X)
-        # if a word was never seen - it must be at least as infrequent
-        # as any of the known words - so the default idf is the max of
-        # known idf's
-        max_idf = max(tfidf.idf_)
-        self.word2weight = defaultdict(
-            lambda: max_idf,
-            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
-
-        return self
-
-    def transform(self, X):
-        return np.array([
-                np.mean([self.word2vec[w] * self.word2weight[w]
-                         for w in words if w in self.word2vec] or
-                        [np.zeros(self.dim)], axis=0)
-                for words in X
-            ])
 
 
 def to_sentences(abstracts, senteces_max=None):
@@ -142,7 +92,6 @@ def abstracts_to_sentences(abstracts, labels):
 def extract_features_we(X_sentences, model, model_size, vocabulary):
     features = []
     for s in X_sentences:
-        # n = 0
         sentence_feature = [0] * model_size
         sentences = str(s).split()
         for word in sentences:
@@ -290,4 +239,4 @@ def classificador():
 
 # reload(sys)
 # sys.setdefaultencoding('utf8')
-classificador()
+# classificador()
